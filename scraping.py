c232dd72ef9ed5750adfeb05fa979dbd4a5669b4
Geoffrey <geoffrey_white@live.com>
2022-02-20 15:15:09 +0800

    Trophies Scraping Fully Completed

--- scraping.py
+++ scraping.py
@@ -12,67 +12,72 @@ buildings = ["Farms", "Inns", "Blacksmiths", "Warrior Barracks", "Knights Jousts
              "Deep Mines", "Stone Pillars", "Alchemist Labs", "Monasteries", "Labyrinths", "Iron Strongholds",
              "Ancient Pyramids", "Halls of Legends"]
 
+# printing of a single table
 def print_table(table):
-    print(table.th.text.strip())
-    print("{:<40}{}".format("Name", "Unlock Requirement"))
-    for i in range(0, 58):
-        print("-", end='')
-    print()
-    for row in table.find_all('tr'):
 
-        for item in row.find_all('th', recursive=False):
-            itemText = item.text.strip()
+    # for all building trophies
+    for build in table.find_all('table'):
+        print(build.th.text.strip())
+        print("{:<40}{}".format("Name", "Unlock Requirement"))
+        for i in range(0, 58):
+            print("-", end='')
+        print()
+        if build.tr.text.strip() in buildings:
+            for e in build.find_all('tr'):
+                count = False
+                for z in e.find_all('td'):
+                    if len(z.text.strip()) != 0:
+                        
+                        print("{:<40}".format(z.text.strip()), end='')
+                        count = True
+                
+                if count:
+                    print()
+            print("\n")
 
-            # remove breaks
-            for e in item.find_all('br'):
-                e.extract()
-            # remove paragraphs
-            for i in item.find_all('p'):
-                PA = i.extract().text.strip()
-                itemText = item.text.strip() + PA
-            # remove paragraphs in <td>
-            for desc in row.find_all('td'):
-                for g in desc.find_all('p'):
-                    description = g.extract().text.strip()
+    # for all trophies other than building trophies
+    if table.th.text.strip() not in buildings and table.th.text.strip() != "Building Types - 25x22 Trophies":
+        print(table.th.text.strip())
+        print("{:<40}{}".format("Name", "Unlock Requirement"))
+        for i in range(0, 58):
+            print("-", end='')
+        print()
 
-            if itemText != '':
-                try:
-                    print("{:<40}{}".format(itemText, row.td.text.strip()))
-                except Exception as e:
-                    if table.tr.text.strip() in buildings:
-                        c = 0
-                        for e in table.find_all('tr'):
-                            for z in e.find_all('td'):
-                                #print(z)
-                                try:
-                                    if len(z.text.strip()) != 0:
-                                        print("{:<40}".format(z.text.strip()), end='')
-                                        # print(z.extract())
-                                except:
-                                    pass
-                            print()
-                        break
+        for row in table.find_all('tr'):
 
+            for item in row.find_all('th', recursive=False):
+                itemText = item.text.strip()
 
-                    pass
+                # remove breaks
+                for e in item.find_all('br'):
+                    e.extract()
+                # remove paragraphs
+                for i in item.find_all('p'):
+                    PA = i.extract().text.strip()
+                    itemText = item.text.strip() + PA
+                # remove paragraphs in <td>
+                for desc in row.find_all('td'):
+                    for g in desc.find_all('p'):
+                        description = g.extract().text.strip()
+
+                if itemText != '':
+                    try:
+                        print("{:<40}{}".format(itemText, row.td.text.strip()))
+
+                    # for building trophies since it is a nested list
+                    except Exception as e:
+                        pass
+    else:
+        return
+
 
     print("\n")
 
 
-
-for t in all_table:
-    print_table(t)
-
-#print(soup.find('table',id=buildings[0]).th.text)
+for t in range(1, len(all_table)-2):
+    print_table(all_table[t])
 
 
-#print(onlybuild.text.strip(), end='')
 
-# print(row.text.strip(), end='')
-'''onlybuild = soup.find('table', id='Farms')
-for e in onlybuild.find_all('tr'):
-    for g in e.find_all('td'):
-        print("{:<40}".format(g.text.strip()), end='')
-    print()'''
 
 
